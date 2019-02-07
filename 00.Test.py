d9500294b06afe938487356dd7049c9fb60f6777
ngochung207 <ngochung207@gmail.com>
2019-02-07 11:01:19 +0700

    update nội dung chưa giải quyết

--- 00.Test.py
+++ 00.Test.py
@@ -1,3 +1,17 @@
-x = input("x: ")
-y = int(x) + 1
-print(f"{x} + 1 = {y}")
+import pandas as pd
+
+df = pd.read_csv('HoaDonSuDung.csv')
+
+SuDung = df['HoaDon']
+ChuoiSuDung = []
+
+PhatHanh = [k for k in range(1,10)]
+
+for i in range(0,len(SuDung)):
+    ChuoiSuDung.append(df['HoaDon'][i])
+
+XoaBo = []
+for j in range(0,len(PhatHanh)):
+    if PhatHanh[j] not in ChuoiSuDung:
+        XoaBo.append(PhatHanh[j])
+print(XoaBo)
--- 01.TinhHinhSuDungHoaDon.py
+++ 01.TinhHinhSuDungHoaDon.py
@@ -42,3 +42,8 @@ print("Chuoi xoa bo: %s" %ChuoiXoaBo)
 print("So luong hoa don xoa bo: %s" %(slXoaBo))
 print("Chuoi chua dung: %s" %ChuoiChuaDung)
 print("So luong hoa don chua dung: %s" %len(ChuoiChuaDung))
+'''
+Các vấn đề chưa giải quyết được:
+    1. Dãy nhập vào không theo thứ tự, thì ChuoiChuaSuDung sẽ bị lấy sai.
+    2. Chưa giải quyết được bài toán 1 dãy phát hành có nhiều dải sử dụng.
+'''
