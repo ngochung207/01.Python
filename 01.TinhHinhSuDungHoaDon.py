62e2357dad3ba49d5157735d8795608ba07ac6bb
ngochung207 <ngochung207@gmail.com>
2019-02-07 10:56:05 +0700

    update: dung vong lap de tao ra chuoi hoa don tu csv nhap vao

--- 01.TinhHinhSuDungHoaDon.py
+++ 01.TinhHinhSuDungHoaDon.py
@@ -1,21 +1,44 @@
-# Nhap vao so hoa don dau.
+# Nhập vào số đầu và số cuối của dẫy hóa đơn phát hành.
 SoDau = int(input("Nhap vao so dau: "))
 SoCuoi = int(input("Nhap vao so cuoi: "))
 
-# Day so phat hanh
-PhatHanh = {i for i in range(SoDau, SoCuoi + 1)}
-slPhatHanh = len(PhatHanh)
+# Tạo ra chuỗi hóa đơn phát hành từ dữ liệu người dùng nhập.
+ChuoiPhatHanh = [i for i in range(SoDau, SoCuoi + 1)]
+slPhatHanh = len(ChuoiPhatHanh)
 
-# Day Su dung
-SuDung = {1,2,3}
-slSuDung = len(SuDung)
+# Import dữ liệu từ file csv các hóa đơn sử dụng.
+import pandas as pd
+ListSuDung = pd.read_csv('HoaDonSuDung.csv')
 
-# Day xoa bo
-XoaBo = PhatHanh - SuDung ''' Sai: truong hop hoa don chua su dung dang tinh la hoa don xoa bo, xem lai'''
-slXoaBo = len(XoaBo)
-# in ra ket qua
-print('{:-^20}'.format("Ket Qua"))
-print("So luong hoa don phat hanh: %s" %(slPhatHanh))
-print("So luong hoa don su dung: %s" %(slSuDung))
+# Tạo ra chuối hóa đơn sử dụng từ dữ liệu nhập vào.
+ChuoiSuDung = []
+for i in range(0, len(ListSuDung)):
+    ChuoiSuDung.append(ListSuDung['HoaDon'][i])
+
+# Đếm số lượng hóa đơn sử dụng.
+slSuDung = len(ChuoiSuDung)
+
+
+# Tìm vị trí của số hóa đơn lớn nhất của chuỗi hóa đơn sử dụng trong chuỗi hóa đơn phát hành.
+SoChuaDung = ChuoiPhatHanh.index(max(ChuoiSuDung))
+ChuoiChuaDung = ChuoiPhatHanh[SoChuaDung+1:None]
+
+
+# Tìm các hóa đơn xóa bỏ
+ChuoiXoaBo = []
+for i in range(0, len(ChuoiPhatHanh[0:SoChuaDung+1])):
+    if  ChuoiPhatHanh[i] not in ChuoiSuDung:
+        ChuoiXoaBo.append(ChuoiPhatHanh[i])
+
+slXoaBo = len(ChuoiXoaBo)
+
+# In ra kết quả
+print('{:-^80}'.format("Ket Qua"))
+print("Chuoi phat hanh: %s" %ChuoiPhatHanh)
+print("So luong hoa don phat hanh: %s" %slPhatHanh)
+print("Chuoi su dung: %s" %ChuoiSuDung)
+print("So luong hoa don su dung: %s" %slSuDung)
+print("Chuoi xoa bo: %s" %ChuoiXoaBo)
 print("So luong hoa don xoa bo: %s" %(slXoaBo))
-print("Cac so xoa bo: %s" %(XoaBo))
+print("Chuoi chua dung: %s" %ChuoiChuaDung)
+print("So luong hoa don chua dung: %s" %len(ChuoiChuaDung))
